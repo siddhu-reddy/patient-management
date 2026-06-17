caea7e95e1e0a6f14b536a5d78a1cc390e3ca684
siddhu-reddy <aletisiddhureddy@gmail.com>
2026-06-17 13:52:56 +0530

    Add gRPC/Kafka support; billing Dockerfile
    Integrates gRPC and Kafka into patient-service and adds a Dockerfile for billing-service. Adds protobuf definitions (billing_service.proto, patient_event.proto), a BillingServiceGrpcClient to call the billing service, and a kafkaProducer to publish PatientEvent messages to the "patient" topic. Updates patient-service pom.xml to include gRPC/protobuf/kafka dependencies and protobuf-maven-plugin (with os-maven-plugin) for codegen. PatientService now creates a billing account via gRPC after saving a new patient. Also adds sample HTTP/GRPC request files and a patient-service module entry in IDE config; updates local Docker/Postgres metadata files and postmaster.pid.

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -4,6 +4,7 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -19,9 +20,12 @@ import static com.pm.patientservice.repository.PatientRepository.*;
 
 @Service
 public class PatientService {
-    private PatientRepository patientRepository;
-    public PatientService(PatientRepository patientRepository){
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+
+    public PatientService(PatientRepository patientRepository,BillingServiceGrpcClient billingServiceGrpcClient){
         this.patientRepository=patientRepository;
+        this.billingServiceGrpcClient=billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatients(){
@@ -34,6 +38,8 @@ public class PatientService {
             throw new EmailAlreadyExistsException("Apatient with this email already exists"+ patientRequestDTO.getEmail());
         }
         Patient newPatient=patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),newPatient.getName(),newPatient.getEmail());
         return PatientMapper.toDTO(newPatient);
     }
 
